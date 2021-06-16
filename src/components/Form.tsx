dee1ab059b22aa796054d506fb1411abb50a1350
iad <node@localhost>
2021-06-17 00:58:01 +0900

    refactor: リファクタリング

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,17 +2,19 @@ import React from 'react';
 import { FormGroup, FormControl, Button } from 'react-bootstrap';
 
 type FormProps = {
+  city: string;
   setCity: React.Dispatch<React.SetStateAction<string>>;
   getWeather: (e: React.FormEvent) => void;
 };
 
-const Form: React.FC<FormProps> = ({ setCity, getWeather }) => {
+const Form: React.FC<FormProps> = ({ city, setCity, getWeather }) => {
   return (
     <form onSubmit={(e) => getWeather(e)}>
       <FormGroup>
         <FormControl
           type="text"
           placeholder="都市名"
+          value={city}
           onChange={(e) => setCity(e.target.value)}
         />
       </FormGroup>
